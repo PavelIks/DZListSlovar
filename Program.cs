a744de64f358610aac47f207ba9be9da83d0ad64
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-10-04 13:04:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dzshka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<C_Dictionary> list_dictionary = new();
+            List<C_Dictionary> list_dictionary = new List<C_Dictionary>();
+
+            list_dictionary.Add(new C_Dictionary("hi", "дарова"));
+            list_dictionary.Add(new C_Dictionary("dima", "дима"));
+            list_dictionary.Add(new C_Dictionary("maks", "макс"));
+            list_dictionary.Add(new C_Dictionary("button", "батон"));
+            list_dictionary.Add(new C_Dictionary("java", "жаба"));
+            list_dictionary.Add(new C_Dictionary("namespace", "имя космоса"));
+
+            foreach (var item in list_dictionary)
+            {
+                Console.WriteLine(item.word1 + "\t\t" + item.word2);
+            }
+        }
+    }
+
+    class C_Dictionary
+    {
+        public C_Dictionary(string _word1, string _word2)
+        {
+            word1 = _word1;
+            word2 = _word2;
+        }
+
+        public string word1;
+        public string word2;
+    }
+}
